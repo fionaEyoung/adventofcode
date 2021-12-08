2521bea1c7e3ab200a5b480c10ba342040256de3
Fiona Young <fiona.young.15@ucl.ac.uk>
2021-12-08 22:35:28 +0000

    Part 2 solution

--- course.py
+++ course.py
@@ -4,16 +4,18 @@ import csv
 
 depth = 0
 position = 0
-
+aim = 0
 with open('input.txt') as f:
 	r = csv.reader(f, delimiter=' ')
 	for row in r:
+		x = int(row[1])
 		if row[0] == 'forward':
-			position += int(row[1])
+			position += x
+			depth += aim * x
 		elif row[0] == 'up':
-			depth -= int(row[1])
+			aim -= x
 		elif row[0] == 'down':
-			depth += int(row[1])
+			aim += x
 
 print("Final depth: ", depth)
 print("Final position: ", position)
