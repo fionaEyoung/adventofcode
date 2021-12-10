8db62ba46f464cdc2c04fc20088fcae17d1ca650
Fiona Young <fiona.young.15@ucl.ac.uk>
2021-12-10 10:43:37 +0000

    Final solution part 2

--- power.py
+++ power.py
@@ -21,3 +21,48 @@ power = (int( '0b' + ''.join(['1' if x else '0' for x in gamma]), 2) *
 	 int( '0b' + ''.join(['1' if x else '0' for x in epsilon]), 2) )
 
 print("Power = ", power)
+
+
+def ox_rating(A, bit):
+	
+	if A.shape[0] == 1:
+		return A[0]
+	else:
+		most_true = A[:,bit].sum() >= (A.shape[0] / 2)
+		if most_true:
+			new = A[ A[:, bit], :  ]    	
+		else:
+			new = A[ np.logical_not(A[:,bit]), :]
+		
+		return ox_rating(new, bit+1)
+
+
+def co2_rating(A, bit):
+	
+	if A.shape[0] == 1:
+		return A[0]
+	else:
+		most_false = A[:,bit].sum() < (A.shape[0] / 2)
+		
+		if most_false:
+			new = A[ A[:, bit], :  ]    	
+		else:
+			new = A[ np.logical_not(A[:,bit]), :]
+		
+		return co2_rating(new, bit+1)
+
+
+o = ox_rating(data, 0)
+c = co2_rating(data, 0)
+
+print("Oxygen rating: ", o)
+print("CO2 rating: ", c)
+
+
+
+life_support = (int( '0b' + ''.join(['1' if x else '0' for x in o]), 2) *
+	 	int( '0b' + ''.join(['1' if x else '0' for x in c]), 2) )
+
+print("Life support rating: ", life_support)
+
+
